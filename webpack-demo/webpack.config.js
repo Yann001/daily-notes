42e68a7b4e6b9f56dc76d6f48dd780fc44e6e829
Yann001 <1156203992@qq.com>
2017-08-06 01:09:12 +0800

    add some webpack basic

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -8,4 +8,11 @@ module.exports = {
   // 生成Source Maps（使调试更容易）
   // 配置生成Source Maps，选择合适的选项[source-map|cheap-module-source-map|eval-source-map|cheap-module-eval-source-map]
   devtool: 'eval-source-map',
+  devServer: {
+    port: 8080, // 设置监听端口
+    contentBase: './public', // 本地服务器所加载页面所在目录
+    colors: true, // 终端输出结果为彩色
+    historyApiFallback: true, // 不跳转
+    inline: true // 实时刷新
+  }
 }
